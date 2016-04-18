dc181c529a7a1e8c28c777a0af361c8865538367
Barsanuphe <paixetprosperite@gmail.com>
2016-04-19 01:02:23 +0200

    Added searching for bookid on goodreads from author+title, to get info later, + tests.

--- book/goodreads.go
+++ book/goodreads.go
@@ -6,16 +6,31 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
+
+	"html"
 
 	h "github.com/barsanuphe/endive/helpers"
 )
 
-const apiRoot = "http://www.goodreads.com/"
+const apiRoot = "https://www.goodreads.com/"
 
 type Response struct {
-	Book GoodreadsBook `xml:"book"`
+	Book   GoodreadsBook   `xml:"book"`
+	Search GoodreadsSearch `xml:"search"`
+}
+
+type GoodreadsSearch struct {
+	ResultsNumber string          `xml:"total-results"`
+	Works         []GoodreadWorks `xml:"results>work"`
+}
+
+type GoodreadWorks struct {
+	ID     string `xml:"best_book>id"`
+	Title  string `xml:"best_book>title"`
+	Author string `xml:"best_book>author>name"`
 }
 
 type GoodreadSeries struct {
@@ -30,17 +45,18 @@ type GoodreadAuthor struct {
 }
 
 type GoodreadsBook struct {
-	ID          string           `xml:"id"`
-	Title       string           `xml:"work>original_title"`
-	ImageURL    string           `xml:"image_url"`
-	NumPages    string           `xml:"num_pages"`
-	Format      string           `xml:"format"`
-	Authors     []GoodreadAuthor `xml:"authors>author"`
-	ISBN        string           `xml:"isbn"`
-	Year        string           `xml:"publication_year"`
-	Description string           `xml:"description"`
-	Series      []GoodreadSeries `xml:"series_works>series_work"`
-	Rating      string           `xml:"average_rating"`
+	ID            string           `xml:"id"`
+	Title         string           `xml:"title"`
+	OriginalTitle string           `xml:"work>original_title"`
+	ImageURL      string           `xml:"image_url"`
+	NumPages      string           `xml:"num_pages"`
+	Format        string           `xml:"format"`
+	Authors       []GoodreadAuthor `xml:"authors>author"`
+	ISBN          string           `xml:"isbn"`
+	Year          string           `xml:"work>original_publication_year"`
+	Description   string           `xml:"description"`
+	Series        []GoodreadSeries `xml:"series_works>series_work"`
+	Rating        string           `xml:"average_rating"`
 }
 
 func (b GoodreadsBook) Author() GoodreadAuthor {
@@ -55,7 +71,10 @@ func (b GoodreadsBook) SeriesString() string {
 }
 
 func (b GoodreadsBook) FullTitle() string {
-	return fmt.Sprintf("%s (%s) %s [%s]", b.Author().Name, b.Year, b.Title, b.SeriesString())
+	if len(b.Series) != 0 {
+		return fmt.Sprintf("%s (%s) %s [%s]", b.Author().Name, b.Year, b.OriginalTitle, b.SeriesString())
+	}
+	return fmt.Sprintf("%s (%s) %s", b.Author().Name, b.Year, b.OriginalTitle)
 }
 
 //------------------------
@@ -89,7 +108,43 @@ func xmlUnmarshal(b []byte, i interface{}) {
 func GetBook(id, key string) GoodreadsBook {
 	defer h.TimeTrack(time.Now(), "Getting Book info")
 	uri := apiRoot + "book/show/" + id + ".xml?key=" + key
-	response := &Response{}
-	getData(uri, response)
+	response := Response{}
+	getData(uri, &response)
 	return response.Book
+}
+
+func makeSearchQ(parts ...string) (query string) {
+	query = strings.Join(parts, "+")
+	r := strings.NewReplacer(" ", "+")
+	return html.EscapeString(r.Replace(query))
+}
+
+func GetBookIDByQuery(author, title, key string) (id string) {
+	defer h.TimeTrack(time.Now(), "Getting Book ID")
+
+	uri := apiRoot + "search/index.xml?key=" + key + "&q=" + makeSearchQ(author, title)
+	response := Response{}
+	getData(uri, &response)
+	// parsing results
+	hits, err := strconv.Atoi(response.Search.ResultsNumber)
+	if err != nil {
+		fmt.Println("error")
+	}
+	if hits != 0 {
+		for _, work := range response.Search.Works {
+			if work.Author == author && work.Title == title {
+				return work.ID
+			}
+		}
+		fmt.Println("Could not find exact match, returning first hit.")
+		return response.Search.Works[0].ID
+	}
+	return
+}
+
+// GetBookIDByISBN using search
+func GetBookIDByISBN(isbn, key string) (id string) {
+	defer h.TimeTrack(time.Now(), "Getting Book ID")
+	// TODO
+	return
 }
--- book/goodreads_test.go
+++ book/goodreads_test.go
@@ -6,14 +6,25 @@ import (
 )
 
 var grBooks = []struct {
-	gr_id             string
+	author            string
+	title             string
+	expectedID        string
 	expectedYear      string
 	expectedFullTitle string
 }{
 	{
+		"Scott Lynch",
+		"The Thorn of Emberlain",
 		"8074907",
 		"2016",
 		"Scott Lynch (2016) The Thorn of Emberlain [Gentleman Bastard #4]",
+	},
+	{
+		"George Orwell",
+		"Animal Farm: A Fairy Story",
+		"7613",
+		"1945",
+		"George Orwell (1945) Animal Farm: A Fairy Story",
 	},
 }
 
@@ -27,15 +38,25 @@ func TestGoodReads(t *testing.T) {
 	}
 
 	for _, book := range grBooks {
-		b := GetBook(book.gr_id, key)
-
+		// getting book_id
+		bookID := GetBookIDByQuery(book.author, book.title, key)
+		if bookID != book.expectedID {
+			t.Errorf("Bad book id, got %s, expected %s.", bookID, book.expectedID)
+		}
+		// getting book information from book_id
+		b := GetBook(bookID, key)
+		if b.Author().Name != book.author {
+			t.Errorf("Bad author, got %s, expected %s.", b.Author().Name, book.author)
+		}
+		if b.Title != book.title && b.OriginalTitle != book.title {
+			t.Errorf("Bad title, got %s / %s, expected %s.", b.Title, b.OriginalTitle, book.title)
+		}
 		if b.Year != book.expectedYear {
 			t.Errorf("Bad year, got %s, expected %s.", b.Year, book.expectedYear)
 		}
 		if b.FullTitle() != book.expectedFullTitle {
 			t.Errorf("Bad title, got %s, expected %s.", b.FullTitle(), book.expectedFullTitle)
-			t.Errorf("Bad title, got %s, expected %s.", b.FullTitle(), book.expectedFullTitle)
 		}
-
 	}
+
 }
