821f20b229eed1adecec12251978f8c5fe185e2e
Barsanuphe <paixetprosperite@gmail.com>
2016-04-23 10:10:02 +0200

    Replace metadata by info, which can hold info from epub or goodreads and is stored in db directly. Updated tests.

--- book/info.go
+++ book/info.go
@@ -3,43 +3,106 @@ package book
 import (
 	"fmt"
 	"strings"
+
+	cfg "github.com/barsanuphe/endive/config"
+	h "github.com/barsanuphe/endive/helpers"
 )
 
 // Info contains all of the known book metadata.
 type Info struct {
-	ID            string   `xml:"id"`
-	Title         string   `xml:"title"`
-	OriginalTitle string   `xml:"work>original_title"`
-	ImageURL      string   `xml:"image_url"`
-	NumPages      string   `xml:"num_pages"`
-	Authors       []string `xml:"authors>author>name"`
-	ISBN          string   `xml:"isbn"`
-	Year          string   `xml:"work>original_publication_year"`
-	Description   string   `xml:"description"`
-	Series        Series   `xml:"series_works>series_work"`
-	AverageRating string   `xml:"average_rating"`
-	Tags          Tags     `xml:"popular_shelves>shelf"`
-}
-
-// Author return a GoodreadsBook's main author.
-func (b Info) Author() string {
-	return b.Authors[0]
-}
-
-// MainSeries return a GoodreadsBook's main series.
-func (b Info) MainSeries() SingleSeries {
-	return b.Series[0]
-}
-
-// SeriesString returns a representation of a GoodreadsBook's main series.
-func (b Info) SeriesString() string {
-	return fmt.Sprintf("%s #%s", strings.TrimSpace(b.MainSeries().Name), b.MainSeries().Position)
+	ID            string   `json:"-" xml:"id"` // TODO see if useful to even parse from xml
+	MainTitle     string   `json:"title" xml:"title"`
+	OriginalTitle string   `json:"original_title" xml:"work>original_title"`
+	ImageURL      string   `json:"image_url" xml:"image_url"`
+	NumPages      string   `json:"num_pages" xml:"num_pages"`
+	Authors       []string `json:"authors" xml:"authors>author>name"`
+	ISBN          string   `json:"isbn" xml:"isbn"`
+	Year          string   `json:"year" xml:"work>original_publication_year"`
+	Description   string   `json:"description" xml:"description"`
+	Series        Series   `json:"series" xml:"series_works>series_work"`
+	AverageRating string   `json:"average_rating" xml:"average_rating"`
+	Tags          Tags     `json:"tags" xml:"popular_shelves>shelf"`
+	Language      string   `json:"-" xml:"TODO: FIND XML PATH"`
 }
 
 // String returns a representation of a GoodreadsBook
-func (b Info) String() string {
-	if len(b.Series) != 0 {
-		return fmt.Sprintf("%s (%s) %s [%s]", b.Author(), b.Year, b.OriginalTitle, b.SeriesString())
+func (i *Info) String() string {
+	if len(i.Series) != 0 {
+		return fmt.Sprintf("%s (%s) %s [%s]", i.Author(), i.Year, i.Title(), i.MainSeries().String())
 	}
-	return fmt.Sprintf("%s (%s) %s", b.Author(), b.Year, b.OriginalTitle)
+	return fmt.Sprintf("%s (%s) %s", i.Author(), i.Year, i.Title())
+}
+
+// HasAny checks if metadata was parsed.
+func (i *Info) HasAny() (hasInfo bool) {
+	// TODO: better check
+	// if Info does not have a title, chances are it's empty.
+	if i.Title() != "" {
+		return true
+	}
+	return
+}
+
+// Title returns Info's main title.
+func (i *Info) Title() string {
+	if i.OriginalTitle != "" {
+		return i.OriginalTitle
+	}
+	return i.MainTitle
+}
+
+// Clean cleans up the Info
+func (i *Info) Clean()  {
+	// default year
+	if i.Year == "" {
+		i.Year = "XXXX"
+	}
+	// clean tags
+	i.Tags.Clean()
+	// clean series
+	for j := range i.Series {
+		i.Series[j].Name = strings.TrimSpace(i.Series[j].Name)
+	}
+}
+
+// Author returns Info's main author.
+func (i *Info) Author() (author string) {
+	author = "Unknown"
+	if len(i.Authors) != 0 {
+		author = i.Authors[0]
+	}
+	return
+}
+
+// MainSeries return the main Series of Info.
+func (i *Info) MainSeries() SingleSeries {
+	if len(i.Series) != 0 {
+		return i.Series[0]
+	}
+	return SingleSeries{}
+}
+
+// IsSimilar checks if metadata is similar to known Info.
+func (i *Info) IsSimilar(o Info) (isSimilar bool) {
+	// TODO do much better, try with isbn if available on both sides
+	// similar == same author/title, for now
+	if i.Author() == o.Author() && i.Title() == o.Title() {
+		return true
+	}
+	return
+}
+
+// Refresh updates Info fields, using the configuration file.
+func (i *Info) Refresh(c cfg.Config) (hasChanged bool) {
+	// for now, only taking into account author aliases
+	for j, author := range i.Authors {
+		for mainalias, aliases := range c.AuthorAliases {
+			_, isIn := h.StringInSlice(author, aliases)
+			if isIn {
+				i.Authors[j] = mainalias
+				break
+			}
+		}
+	}
+	return
 }
--- book/info_test.go
+++ book/info_test.go
@@ -1,1 +1,61 @@
 package book
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestInfo(t *testing.T) {
+	fmt.Println("+ Testing Epub.GetMetaData()...")
+	for i, testEpub := range epubs {
+		e := NewBook(i, testEpub.filename, standardTestConfig, true)
+
+		// testing HasAny
+		hasMetadata := e.Metadata.HasAny()
+		if hasMetadata {
+			t.Errorf("Error: %s should not have metadata yet.", e.FullPath())
+		}
+		// reading info
+		info, err := e.MainEpub().ReadMetadata()
+		if err != nil {
+			t.Errorf("Error getting Metadata for %s, got %s, expected nil", e.FullPath(), err)
+		}
+		e.EpubMetadata = info
+		e.Metadata = info
+
+		// testing Get, GetFirstValue
+		if e.Metadata.Title() != testEpub.expectedTitle {
+			t.Errorf("GetMetadata(%s) returned %s, expected %s!", testEpub.filename, e.Metadata.Title(), testEpub.expectedTitle)
+		}
+		if e.Metadata.Author() != testEpub.expectedAuthor {
+			t.Errorf("GetMetadata(%s) returned %s, expected %s!", testEpub.filename, e.Metadata.Author(), testEpub.expectedAuthor)
+		}
+		if e.Metadata.Year != testEpub.expectedPublicationYear {
+			t.Errorf("GetMetadata(%s) returned %s, expected %s!", testEpub.filename, e.Metadata.Year, testEpub.expectedPublicationYear)
+		}
+		if e.Metadata.Language != testEpub.expectedLanguage {
+			t.Errorf("GetMetadata(%s) returned %s, expected %s!", testEpub.filename, e.Metadata.Language, testEpub.expectedLanguage)
+		}
+
+		// testing HasAny
+		hasMetadata = e.Metadata.HasAny()
+		if !hasMetadata {
+			t.Errorf("Error: %s should have metadata by now.", e.FullPath())
+		}
+
+		// testing IsSimilar
+		o := Info{}
+		if e.Metadata.IsSimilar(o) {
+			t.Errorf("Error: metadata should not be similar.")
+		}
+		// copying manually
+		o.Authors = []string{}
+		o.Authors = append(o.Authors, e.Metadata.Authors...)
+		o.MainTitle = e.Metadata.MainTitle
+		// checking again
+		if !e.Metadata.IsSimilar(o) {
+			t.Errorf("Error: metadata should be similar.")
+		}
+
+	}
+}
