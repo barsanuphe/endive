b01754f383c1e4751c82c5897a9e7fb795dd3991
Barsanuphe <paixetprosperite@gmail.com>
2016-05-02 23:10:42 +0200

    Used assert package to simplify tests.

--- config/config_test.go
+++ config/config_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	h "github.com/barsanuphe/endive/helpers"
+	"github.com/stretchr/testify/assert"
 )
 
 var configFile = "../test/config.yaml"
@@ -28,48 +29,27 @@ func TestMain(m *testing.M) {
 
 func TestConfigLoad(t *testing.T) {
 	fmt.Println("+ Testing Config.Load()...")
+	assert := assert.New(t)
 	c := Config{Filename: configFile}
 
 	err := c.Load()
-	if err != nil {
-		t.Errorf("Error loading configuration file: %s", err.Error())
-	}
-	if c.LibraryRoot != "test_library" {
-		t.Errorf("Error loading library root: %s instead of %s", c.LibraryRoot, "test_library")
-	}
-	if c.DatabaseFile != "test_library/endive.json" {
-		t.Errorf("Error loading database filename: %s instead of %s", c.DatabaseFile, "test_library/endive.json")
-	}
-	if len(c.RetailSource) != 2 {
-		t.Errorf("Error: loading retail sources, expected 2 instead of %d", len(c.RetailSource))
-	}
-	if len(c.NonRetailSource) != 1 {
-		t.Errorf("Error: loading retail sources, expected 1 instead of %d", len(c.NonRetailSource))
-	}
-	if len(c.AuthorAliases) != 2 {
-		t.Errorf("Error: loading author aliases, expected 2 instead of %d", len(c.AuthorAliases))
-	}
-	if len(c.AuthorAliases["China Miéville"]) != 2 {
-		t.Errorf("Error: loading author aliases for china miéville, should have gotten 2 instead of %d", len(c.AuthorAliases["China Miéville"]))
-	}
+	assert.Nil(err, "Error loading configuration file")
+	assert.Equal(c.LibraryRoot, "test_library", "Error loading library root")
+	assert.Equal(c.DatabaseFile, "test_library/endive.json", "Error loading database filename")
+	assert.Equal(len(c.RetailSource), 2, "Error: loading retail sources, expected 2")
+	assert.Equal(len(c.NonRetailSource), 1, "Error: loading retail sources, expected 1")
+	assert.Equal(len(c.AuthorAliases), 2, "Error: loading author aliases, expected 2")
+	assert.Equal(len(c.AuthorAliases["China Miéville"]), 2, "Error: loading author aliases for china miéville, should have gotten 2")
 	// checking library root, expecting error
 	err = c.Check()
-	if err == nil {
-		t.Errorf("Error checking configuration file, library root should not exist.")
-	}
+	assert.NotNil(err, "Error checking configuration file, library root should not exist.")
 	// library root creation
 	err = os.Mkdir(c.LibraryRoot, 0777)
-	if err != nil {
-		t.Errorf("Error creating library root")
-	}
+	assert.Nil(err, "Error creating library root")
 	// check should be ok
 	err = c.Check()
-	if err != nil {
-		t.Errorf("Error checking configuration file: %s", err.Error())
-	}
+	assert.Nil(err, "Error checking configuration file")
 	// cleanup
 	err = os.Remove(c.LibraryRoot)
-	if err != nil {
-		t.Errorf("Error removing library root")
-	}
+	assert.Nil(err, "Error removing library root")
 }
--- config/known_hashes_test.go
+++ config/known_hashes_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var hashesFile = "../test/hashes.json"
@@ -11,75 +13,46 @@ var hashesFile = "../test/hashes.json"
 // TestKnownHashes tests for all KnownHashes functions
 func TestKnownHashes(t *testing.T) {
 	fmt.Println("+ Testing KnownHashes...")
+	assert := assert.New(t)
 	c := KnownHashes{Filename: hashesFile}
 
 	// check Load
 	err := c.Load()
-	if err != nil {
-		t.Errorf("Error loading hashes file: %s", err.Error())
-	}
-	if len(c.Hashes) != 2 && c.Count != 2 {
-		t.Errorf("Error loading hashes, should have found 2 hashes instead of %d", len(c.Hashes))
-	}
+	assert.Nil(err, "Error loading hashes file")
+	assert.Equal(len(c.Hashes), 2)
+	assert.Equal(c.Count, 2)
 
 	// check Save before modification
 	wasModified, err := c.Save()
-	if err != nil {
-		t.Errorf("Error saving")
-	}
-	if wasModified {
-		t.Errorf("Nothing has changed, should not have saved")
-	}
+	assert.Nil(err, "Error saving")
+	assert.False(wasModified, "Nothing has changed, should not have saved")
 
 	// check IsIN
 	isIn := c.IsIn("moijmoij")
-	if isIn {
-		t.Errorf("Bad hash cannot be in list")
-	}
+	assert.False(isIn, "Bad hash cannot be in list")
 	isIn = c.IsIn("74657165a56c9a54ed887cd895a0f67a70f29cbecaa96dfda840c76580da3dd8")
-	if !isIn {
-		t.Errorf("Known hash should be detected")
-	}
+	assert.True(isIn, "Known hash should be detected")
 	isIn = c.IsIn("74657165a56c9a54ed887cd895a0f67a70f29cbecaa96dfda840c76580da3dd9")
-	if isIn {
-		t.Errorf("Unknown hash should not be detected")
-	}
+	assert.False(isIn, "Unknown hash should not be detected")
 
 	// check Add
 	added, err := c.Add("moijmoij")
-	if err == nil {
-		t.Errorf("Bad hash should have raised error")
-	}
-	if added {
-		t.Errorf("Bad hash should not have been added")
-	}
+	assert.NotNil(err, "Bad hash should have raised error")
+	assert.False(added, "Bad hash should not have been added")
 	added, err = c.Add("74657165a56c9a54ed887cd895a0f67a70f29cbecaa96dfda840c76580da3dd8")
-	if err != nil {
-		t.Errorf("Hash should not have raised error")
-	}
-	if added {
-		t.Errorf("Known hash should not have been added")
-	}
+	assert.Nil(err, "Hash should not have raised error")
+	assert.False(added, "Known hash should not have been added")
 	added, err = c.Add("74657165a56c9a54ed887cd895a0f67a70f29cbecaa96dfda840c76580da3dd9")
-	if err != nil {
-		t.Errorf("Hash should not have raised error")
-	}
-	if !added {
-		t.Errorf("Unknown hash should have been added")
-	}
+	assert.Nil(err, "Hash should not have raised error")
+	assert.True(added, "Unknown hash should have been added")
 
 	// check Save after modification
 	c.Filename += "_temp"
 	wasModified, err = c.Save()
-	if err != nil {
-		t.Errorf("Error saving")
-	}
-	if !wasModified {
-		t.Errorf("One hash was added, should have saved")
-	}
+	assert.Nil(err, "Error saving")
+	assert.True(wasModified, "One hash was added, should have saved")
+
 	// cleanup
 	err = os.Remove(c.Filename)
-	if err != nil {
-		t.Errorf("Error during cleanup of file " + c.Filename)
-	}
+	assert.Nil(err, "Error during cleanup of file "+c.Filename)
 }
