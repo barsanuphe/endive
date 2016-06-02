64c368ff000369e24a129ba6157a53182aa9ffd9
Barsanuphe <paixetprosperite@gmail.com>
2016-06-02 21:44:44 +0200

    tag is now an alias of tags in search/export.

--- library/search.go
+++ library/search.go
@@ -139,6 +139,7 @@ func (ldb *DB) prepareQuery(queryString string) (newQuery string) {
 		"language:", "metadata.language:",
 		"series:", "metadata.series.seriesname:",
 		"tags:", "metadata.tags.name:",
+		"tag:", "metadata.tags.name:",
 		"publisher:", "metadata.publisher:",
 		"category:", "metadata.category:",
 		"genre:", "metadata.main_genre:",
