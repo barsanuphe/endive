64fc3d3f02df0f38c3b1f0b0fdbc72d0dab0f9fa
Barsanuphe <paixetprosperite@gmail.com>
2016-10-26 22:41:17 +0200

    Minor test cleanup.

--- book/metadata_helpers_test.go
+++ book/metadata_helpers_test.go
@@ -82,38 +82,37 @@ const (
 func TestOptions(t *testing.T) {
 	fmt.Println("+ Testing CleanSliceAndTagEntries/RemoveDuplicates()...")
 	assert := assert.New(t)
-
 	u := &u.UI{}
 
-	t5 := []string{option1, option1, option3, option4, option5, option5}
-	CleanSliceAndTagEntries(u, option1, option3, &t5)
-	assert.Equal(4, len(t5), fmt.Sprintf(expectedOptionsCount, 4))
-	assert.Equal(u.Tag(option1, true), t5[0], expectedLocal)
-	assert.Equal(u.Tag(option3, false), t5[1], expectedRemote)
-	assert.Equal(option4, t5[2], unexpectedOption)
-	assert.Equal(option5, t5[3], unexpectedOption)
+	t1 := []string{option1, option1, option3, option4, option5, option5}
+	CleanSliceAndTagEntries(u, option1, option3, &t1)
+	assert.Equal(4, len(t1), fmt.Sprintf(expectedOptionsCount, 4))
+	assert.Equal(u.Tag(option1, true), t1[0], expectedLocal)
+	assert.Equal(u.Tag(option3, false), t1[1], expectedRemote)
+	assert.Equal(option4, t1[2], unexpectedOption)
+	assert.Equal(option5, t1[3], unexpectedOption)
 
-	t6 := []string{option1, option1, option3, option4, option5, option5}
-	CleanSliceAndTagEntries(u, option1, option1, &t6)
-	assert.Equal(4, len(t6), fmt.Sprintf(expectedOptionsCount, 4))
-	assert.Equal(u.Tag(u.Tag(option1, false), true), t6[0], expectedLocal+" & "+expectedRemote)
-	assert.Equal(option3, t6[1], unexpectedOption)
-	assert.Equal(option4, t6[2], unexpectedOption)
-	assert.Equal(option5, t6[3], unexpectedOption)
+	t2 := []string{option1, option1, option3, option4, option5, option5}
+	CleanSliceAndTagEntries(u, option1, option1, &t2)
+	assert.Equal(4, len(t2), fmt.Sprintf(expectedOptionsCount, 4))
+	assert.Equal(u.Tag(u.Tag(option1, false), true), t2[0], expectedLocal+" & "+expectedRemote)
+	assert.Equal(option3, t2[1], unexpectedOption)
+	assert.Equal(option4, t2[2], unexpectedOption)
+	assert.Equal(option5, t2[3], unexpectedOption)
 
 	// + one tag is not found
-	t7 := []string{option1, option1, option3, option4, option5, option5}
-	CleanSliceAndTagEntries(u, option1, option1+"_", &t7)
-	assert.Equal(4, len(t7), fmt.Sprintf(expectedOptionsCount, 4))
-	assert.Equal(u.Tag(option1, true), t7[0], expectedLocal)
-	assert.Equal(option3, t7[1], unexpectedOption)
-	assert.Equal(option4, t7[2], unexpectedOption)
-	assert.Equal(option5, t7[3], unexpectedOption)
+	t3 := []string{option1, option1, option3, option4, option5, option5}
+	CleanSliceAndTagEntries(u, option1, option1+"_", &t3)
+	assert.Equal(4, len(t3), fmt.Sprintf(expectedOptionsCount, 4))
+	assert.Equal(u.Tag(option1, true), t3[0], expectedLocal)
+	assert.Equal(option3, t3[1], unexpectedOption)
+	assert.Equal(option4, t3[2], unexpectedOption)
+	assert.Equal(option5, t3[3], unexpectedOption)
 
-	t8 := []string{option1, option1, option3, option4, option5, option5}
-	CleanSliceAndTagEntries(u, option1, option1, &t8, option5)
-	assert.Equal(3, len(t8), fmt.Sprintf(expectedOptionsCount, 3))
-	assert.Equal(u.Tag(u.Tag(option1, false), true), t8[0], expectedLocal+" & "+expectedRemote)
-	assert.Equal(option3, t8[1], unexpectedOption)
-	assert.Equal(option4, t8[2], unexpectedOption)
+	t4 := []string{option1, option1, option3, option4, option5, option5}
+	CleanSliceAndTagEntries(u, option1, option1, &t4, option5)
+	assert.Equal(3, len(t4), fmt.Sprintf(expectedOptionsCount, 3))
+	assert.Equal(u.Tag(u.Tag(option1, false), true), t4[0], expectedLocal+" & "+expectedRemote)
+	assert.Equal(option3, t4[1], unexpectedOption)
+	assert.Equal(option4, t4[2], unexpectedOption)
 }
